34375ebcf57e897905493c5f28a2ccb2570b9f89
Thaiana Quadros <thai_quadros@hotmail.com>
2022-04-07 15:23:46 -0400

    removing the sub func only from the context required fields tosignup

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -99,7 +99,7 @@ describe('cadastro', function () {
         })
     })
 
-    context.only('quando nao preencho nenhum dos campos', function () {
+    context('quando nao preencho nenhum dos campos', function () {
         const alertMessages = [
             'Nome é obrigatório',
             'E-mail é obrigatório',
